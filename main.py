f5c82a3fca1733ea376ecda6697b60d2d9c05ded
ElectrWeakHyprCharge <{ID}+{username}@users.noreply.github.com>
2018-10-15 21:43:30 -0300

    Bugfix for main.py: reply to comments failed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #! -*- coding: utf-8 -*-
 
 from unicodedata import normalize
-from typing import Dict, Optional
+from typing import List, Tuple, Optional
 from traceback import print_exc
 from random import choice
 from time import sleep
@@ -20,15 +20,19 @@ with open('data.json', 'r') as f:
     userdata = json.load(f)
 
 
-def load_images(from_path: str, path_prefix: Optional[str]=None):
+def load_images(from_path: str, path_prefix: Optional[str]=None) -> Tuple[str]:
+    """
+    Load a tuple of image links from the file at path_prefix + from_path.
+    path_prefix is by default where the script is located
+    """
     filepath = path.join(
         path_prefix or path.dirname(path.abspath(__file__)),
         from_path
     )
 
     with open(filepath, 'r') as f:
-        return [link[1:] for link in f.read().splitlines()
-                if link.startswith('#')]
+        return tuple([link[1:] for link in f.read().splitlines()
+                if link.startswith('#')])
 
 
 COMMANDS = {
@@ -47,20 +51,27 @@ def match_commands(comment: Comment, accents=True) -> set:
     Return a set with tuples of the form ((reward, reward_images), recipient),
     one for each match in the body of the provided comment.
     """
+    content = comment.body
     if not accents:
-        content = normalize('NFD', comment.body).encode('ascii', 'ignore')
-        content = content.decode('ascii')
-
+        pass
+        #content = normalize('NFD', content).encode('ascii', 'ignore')
+        #content = content.decode('ascii')
+    
     return {
-        (COMMANDS[m.group(1)], m.group(2) or comment.parent().author)
+        (COMMANDS[m.group(1)], m.group(2) or comment.parent().author.name)
         for m in PATTERN.finditer(content)
-    } 
+    }
 
 
-def reply(comment, recipient, sender, times_received, reward, images, retries=5) -> None:
+def reply(comment: Comment,
+          recipient: str,
+          sender: str,
+          times_received: int,
+          reward: str,
+          image: str, 
+          retries: str = 5) -> None:
     msg =(
-        f'#[Aquí está tu {reward}, /u/{recipient}!]'
-        f'({choice(images)} "{reward}")\n\n'
+        f'#[Aquí está tu {reward}, /u/{recipient}!]({image} "{reward}")\n\n'
         f'/u/{recipient} recibió {reward} {times_received} '
         f'{"vez" if times_received == 1 else "veces"}.'
         f'(dado por /u/{sender})'
@@ -88,23 +99,24 @@ def is_valid_command(comment: Comment) -> bool:
         
     print('Matches:',  matches) 
     for command, user in matches:
-        reward, reward_images = COMMANDS[command]
+        reward, reward_images = command
 
         user_cf = user.casefold()
         userdata[reward][user_cf] = userdata[reward].get(user_cf, 0) + 1
         
         reply(
-            comment,
-            user,
-            comment.author.name,
-            userdata[reward][user_cf],
-            reward, reward_images
+            comment=comment,
+            recipient=user,
+            sender=comment.author.name,
+            times_received=userdata[reward][user_cf],
+            reward=reward,
+            image=choice(reward_images)
         )
         sleep(1)
     return True
 
 
-def main():
+def main() -> None:
     for comment in sub.stream.comments():
         if comment.saved: continue
         
